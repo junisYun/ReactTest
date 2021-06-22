e85e125ccaa86d98b6e8f4d0d3e82cace1fc0ba8
junisYun <junis.yun94@gmail.com>
2021-06-23 01:29:02 +0900

    2021_06_23 01:28

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import './App.css';
 import React, { Component } from 'react';
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        World Wide Web!
-      </header>
-    )
-  }
-}
-
-class Category extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
+import Category from './components/Category'
+import Content from './components/Content'
+import Subject from './components/Subject'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      selected_content_id: 2,
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!!' },
+      category: [
+        { id: 1, title: 'HTML', sub: 'HTML is HyperText Markup Language' },
+        { id: 2, title: 'CSS', sub: 'CSS is Cascading Style Sheets' },
+        { id: 3, title: 'JavaScript', sub: 'JavaScript is Programming Language base on Object-Oriented Programming(OOP)' }
+      ]
+    }
+  }
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+        var i = 0;
+        while(i < this.state.category.length) {
+          var data = this.state.category[i];
+          if(data.id === this.state.selected_content_id) {
+            _title = data.title;
+            _desc = data.sub;
+            break;
+          }
+          i++;
+        }
+    }
+    console.log('render', this);
     return (
       <div className="App">
-        <Subject></Subject>
-        <Category></Category>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}>
+        </Subject>
+        <Category data={this.state.category}
+          onChangePage={function (id) {
+            this.setState(
+              {mode:'read', selected_content_id:Number(id)});
+          }.bind(this)}>
+          </Category>
+        <Content title={_title} sub={_desc}></Content>
       </div>
     );
   }
